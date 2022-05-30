e6c549954f26e60712bcb305331d757ba8ebe0de
Eddy Soma <esoma92.dev@gmail.com>
2022-05-30 11:16:26 -0400

    finish subarray sum equals k implementation

--- src/main/java/io/esoma/cbj/prob/SubarraySumEqualsK.java
+++ src/main/java/io/esoma/cbj/prob/SubarraySumEqualsK.java
@@ -1,5 +1,8 @@
 package io.esoma.cbj.prob;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class that solves the problem of counting the total number of sub-arrays (of a given array of
  * integers) whose sums are equal to a preset number K. Each subarray must be non-empty.
@@ -8,7 +11,35 @@ public class SubarraySumEqualsK {
 
   private SubarraySumEqualsK() {}
 
+  /**
+   * Counts the number of sub-arrays whose sums are equal to K. This implementation uses a
+   * prefix-sum hash map (key being the sum, and value being the count) that is built interactively
+   * with the main iteration to quickly compute if any sub-array fulfills the condition.
+   *
+   * @param array the input array
+   * @param k the target sum
+   * @return the total number of sub-arrays that sum up to k
+   */
   public static int count(int[] array, int k) {
-    return 0;
+    int result = 0;
+
+    Map<Integer, Integer> prefixSums = new HashMap<>();
+
+    int curSum = 0;
+    // Add the empty sub-array.
+    prefixSums.put(curSum, 1);
+
+    for (int cur : array) {
+      curSum += cur;
+
+      int curTarget = curSum - k;
+      if (prefixSums.containsKey(curTarget)) {
+        result += prefixSums.get(curTarget);
+      }
+
+      prefixSums.compute(curSum, (key, value) -> value == null ? 1 : value + 1);
+    }
+
+    return result;
   }
 }
